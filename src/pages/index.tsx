298561acecfea0ac312bb74c3db3c579816e91b4
Jake1848 <jake@ironcladroofers.com>
2025-11-02 09:05:22 -0500

    Change default redirect to simpler dashboard
    - Redirect to /app/dashboard instead of /app/ultra-dashboard
    - Simpler dashboard for initial load testing
    - Ultra-dashboard can be accessed directly via URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const router = useRouter()
 
   useEffect(() => {
-    router.push('/app/ultra-dashboard')
+    router.push('/app/dashboard')
   }, [router])
 
   return (
